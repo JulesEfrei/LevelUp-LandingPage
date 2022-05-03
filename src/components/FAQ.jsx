69bc5a46dcfb64c105675037d204da7d52a73f36
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-05-03 16:56:43 +0200

    Fix : NavBar Responsive collision

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -7,7 +7,7 @@ function FAQ() {
 
   const faq = [
     {
-      question : "How to get the application ?",
+      question : "How to get the application?",
       answer: "Sorry, at the moment LevelUp is in a test phase and is only available for certain users. Fill the form below to be informed about the release."
     },
     {
@@ -15,7 +15,7 @@ function FAQ() {
       answer: "For the moment no date has been announced. We are working hard to release the application as soon as possible. Stay patient, we are coming !  "
     },
     {
-      question : "Can they be part of the beta-testers ?",
+      question : "Can they be part of the beta-testers?",
       answer: "We are always listening to your feedback. You can request access to the Beta by filling out the form below."
     },
     // {
