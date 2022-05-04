628ca19488c76ef13fda6ef3b5299eb52cfb3df6
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-05-04 10:07:49 +0200

    Update : Question Style

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -9,10 +9,20 @@ function Question({data}) {
         setDisplay(display == "none" ? "flex" : "none")
     }
 
+    const rounded = {
+        borderBottomLeftRadius: 5,
+        borderBottomRightRadius: 5,
+    }
+
+    const square = {
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+    }
+
   return (
     <div className="question">
         
-        <div className="ask" onClick={toggle}>
+        <div className="ask" onClick={toggle} style={display === "none" ? rounded : square}>
             <button onClick={toggle}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z"/></svg>
             </button>
