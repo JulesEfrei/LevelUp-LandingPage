b9fd8d621bdbbf706e7b34c48182562d8acfa4a0
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-05-04 17:02:01 +0200

    Update : Form Style

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,19 @@
 import React, { useState } from 'react'
 import "../styles/contact.scss"
+import FormTitle from "./FormTitle"
 
 function Form({title, form}) {
 
     const [display, setDisplay] = useState("none")
     const [arrow, setArrow] = useState(0)
+
+    const [formName, setFormName] = useState(form.id)
+
+    const [name, setName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [select, setSelect] = useState("")
+    const [text, setText] = useState("")
 
 
     function toggle() {
@@ -24,29 +33,44 @@ function Form({title, form}) {
         marginBottom: 0
     }
 
+    function verify(event) {
+
+        console.log("In build...")
+
+    //     // Get all enabled input
+    //     const inputEnable = Object.keys(form).slice(1).reduce((prev, curr) =>  [...prev, Object.values(form[curr])[0] && curr ] , []).filter(elm => elm != false)
+
+    //     //Test if one of the required input is empty
+        
+
+    //     //Stop the default event and display error popup
+    //     event.preventDefault()
+
+    }
+
   return (
     <div className="formContainer">
 
             <div className="title" onClick={toggle} style={display === "none" ? rounded : square}>
-                <h2>{title}</h2>
+                <FormTitle title={title} form={form.id} />
                 <button onClick={toggle} style={{ transform: `rotate(${arrow}deg)` }}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z"/></svg>
                 </button>
             </div>
 
             <div className="form" style={{ display: display }}>
-                <form method="post" name={`form-${form.id}`}>
-                    <input type="hidden" name="form-name" value={`form-${form.id}`} />
-                    {form.name.enable == true && ( <input type="text" name="name" id={`name${form.id}`} placeholder="Name" /> )}
-                    {form.lastName.enable == true && ( <input type="text" name="lastName" id={`lastName${form.id}`} placeholder="Last Name" /> )}
-                    {form.email.enable == true && ( <input type="email" name="email" id={`email${form.id}`} placeholder="E-mail" /> )}
+                <form method="post" name={formName} onSubmit={verify}>
+                    <input type="hidden" name="form-name" value={formName} />
+                    {form.name.enable == true && ( <input type="text" name="name" id={`name${form.id}`} placeholder="Name" onChange={(e) => setName(e.target.value)} /> )}
+                    {form.lastName.enable == true && ( <input type="text" name="lastName" id={`lastName${form.id}`} placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} /> )}
+                    {form.email.enable == true && ( <input type="email" name="email" id={`email${form.id}`} placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} /> )}
                     {form.select.enable == true && ( 
-                        <select name="appSelect" id={`appSelect${form.id}`}> 
+                        <select name="appSelect" id={`appSelect${form.id}`} onChange={(e) => setSelect(e.target.value)}> 
                             <option value="app">LevelUp Application</option>
                             <option value="web">LevelUp Web Site</option>
                         </select>
                     )}
-                    {form.message.enable == true && ( <textarea name={form.message.name} id={`text${form.id}`} cols="40" rows="7" placeholder={form.message.placeholder}></textarea> )}
+                    {form.message.enable == true && ( <textarea name={form.message.name} id={`text${form.id}`} cols="40" rows="7" placeholder={form.message.placeholder} onChange={(e) => setText(e.target.value)}></textarea> )}
                     <input type="submit" value="Submit Form" />
                 </form>
             </div>
